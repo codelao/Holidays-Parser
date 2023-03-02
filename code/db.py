3fe724e3917f9e6419680333c9b0b97f2d9987e4
Code Lao <109976097+codelao@users.noreply.github.com>
2023-03-02 16:43:31 +0300

    2.0.0

--- code/db.py
+++ code/db.py
@@ -4,7 +4,8 @@ import sqlite3
 class Database:
     def __init__(self, db_file):
         self.con = sqlite3.connect(db_file, check_same_thread=False)
+        self.con.cursor().execute("CREATE TABLE IF NOT EXISTS holidays(date TEXT PRIMARY KEY, holiday TEXT)")
 
-    def quote_add(self, quote):
+    def holiday_add(self, date, holiday):
         with self.con:
-            self.con.cursor().execute("INSERT INTO quotes VALUES (?)", (quote,))
+            self.con.cursor().execute("INSERT INTO holidays VALUES (?, ?)", (date, holiday,))
