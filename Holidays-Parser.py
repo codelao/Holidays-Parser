f859ebc1fdf995653750cac69a8f6b4d10971af2
Code Lao <arsezsoftware@gmail.com>
2023-08-31 14:47:18 -0500

    small changes

--- Holidays-Parser.py
+++ Holidays-Parser.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python3
+
 import os
-import requests
-import datetime
 import sys
+import time
+import datetime
+import requests
 import colorama
 import urllib.request
-from db import Database
 from bs4 import BeautifulSoup
-from termcolor import colored
+from db import Database
 
 
 CURRENT_PATH = os.path.dirname(__file__)
@@ -14,10 +16,9 @@ db = Database(CURRENT_PATH + '/holidays.db')
 
 class App:
     def __init__(self):
-        self.check_internet_connection()
         self.parser()
 
-    def check_internet_connection():
+    def check_internet_connection(self):
         try:
             urllib.request.urlopen('https://google.com', timeout=1)
             return True
@@ -35,11 +36,13 @@ class App:
             self.holiday = self.soup.find_all('h2')[0].get_text(strip=True)
             db.holiday_add(date=self.date, holiday=self.holiday)
             self.holidays_parsed = db.parsed_holidays()
-            print('Today is ' + colored(self.holiday, 'green') + '\nHolidays parsed: ' + colored(self.holidays_parsed, 'blue'))
-            sys.exit()
+            print('Today is \033[32m' + self.holiday + '\033[0m.' +
+                '\nHolidays parsed: \033[32m' + str(self.holidays_parsed) + '\033[0m')
+            time.sleep(1)
+            sys.exit(0)
         else:
-            print(colored('Check your internet connection', 'red'))
-            sys.exit()
+            print('\033[31mCheck your internet connection or try again.\033[0m')
+            sys.exit(1)
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -8,9 +8,14 @@
 
 
 ## Screenshots
+### Parsed holiday output:
 <p>
-  <img src="READMEimages/screenshot1.png" width="50%">
-  <img src="READMEimages/screenshot2.png" width="50%">
+  <img src="READMEimages/parser.png" width="50%">
+</p>
+
+### Holidays archive in database file:
+<p>
+  <img src="READMEimages/archive.png" width="50%">
 </p>
 
 
@@ -23,7 +28,7 @@ After running the script you will see today's holiday in the output and in the a
 *Note:* you need to have Python3 and Git installed in your system before moving to the installation steps.
 1. Use this command in the Terminal:
 ```
-git clone https://github.com/codelao/Holidays-Parser.git && cd Holidays-Parser && pip3 install -r requirements.txt && chmod +x parser.py
+git clone https://github.com/codelao/Holidays-Parser.git && cd Holidays-Parser && pip3 install -r requirements.txt && chmod +x Holidays-Parser.py
 ```
 2. Finally, now you can use this command everytime you want to parse today's holiday:
 ```
